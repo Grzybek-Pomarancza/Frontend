e3598091fbab0c888399b9d2854627fdcbafb4a3
Olga Zawislak <olga.zawislak99@gmail.com>
2020-04-07 13:57:33 +0200

    login page

---
+++ src/Components/Login.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
+import "./styles/login.css"
+
+export default function Login(props) {
+    return (
+        <div className="Login p-5 bg-light">
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>E-mail</Form.Label>
+                    <Form.Control required type="email" placeholder="janedoe@email.com" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control required type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Remember me" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
+}
