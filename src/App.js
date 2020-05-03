e0c13bceadd65bcace1b88a61cb20737872edc49
MDWojcieszak <wojcieszak.4@gmail.com>
2020-05-03 11:32:38 +0200

    changed routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import Routes from "./Routes";
+import { Switch, Route } from "react-router-dom";
+import LoginView from "./views/LoginView";
+import SignupView from "./views/SignupView";
+import HomeView from "./views/HomeView";
 
 export default class App extends Component {
   constructor(props) {
@@ -29,19 +32,35 @@ export default class App extends Component {
   }
 
   render() {
-    var { isLoaded, items } = this.state;
-    if (!isLoaded) {
-      return (
-        <div>
-          <h1>Loading API...</h1>
+    return (
+      <Router>
+        <div className="auth-wrapper">
+          <Switch>
+            <Route
+              exact
+              path="/(|sign-in)"
+              render={(props) => (
+                <LoginView
+                  {...props}
+                  loggedInStatus={this.state.loggedInStatus}
+                />
+              )}
+            />
+            <Route path="/sign-up" component={SignupView} />
+            <Route path="/home" component={HomeView} />
+            <Route
+              exact
+              path="/home"
+              render={(props) => (
+                <HomeView
+                  {...props}
+                  loggedInStatus={this.state.loggedInStatus}
+                />
+              )}
+            />
+          </Switch>
         </div>
-      );
-    } else {
-      return (
-        <Router>
-          <Routes />
-        </Router>
-      );
-    }
+      </Router>
+    );
   }
 }
