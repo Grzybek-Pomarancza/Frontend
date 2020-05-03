b75f3acf78455950e0bc833ee3c77252bc26695a
Olga Zawislak <olga.zawislak99@gmail.com>
2020-05-03 20:50:10 +0200

    bg blur

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -45,6 +45,7 @@ export default class SignUp extends Component {
               ? "Too short name"
               : ""
             : "First name is required";
+
         break;
       case "lastName":
         errors.lastName =
